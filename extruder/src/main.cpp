57ce2516e73aa6b4e1eca371b8daeb45e484d58a
Florin Tobler <florin.tobler@hotmail.com>
2022-10-01 20:01:21 +0200

    different speeds

--- extruder/src/main.cpp
+++ extruder/src/main.cpp
@@ -23,65 +23,62 @@
 void isr();
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 
-  Timer1.initialize(1000000 / TICKS_PER_SECOND);
-  Timer1.attachInterrupt(isr); // blinkLED to run every 0.15 seconds
+    Timer1.initialize(1000000 / TICKS_PER_SECOND);
+    Timer1.attachInterrupt(isr); // blinkLED to run every 0.15 seconds
 
-  pinMode(EN, OUTPUT);
-  pinMode(STEP, OUTPUT);
-  pinMode(DIR, OUTPUT);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(M1, OUTPUT);
-  pinMode(M2, OUTPUT);
-  pinMode(M3, OUTPUT);
+    pinMode(EN, OUTPUT);
+    pinMode(STEP, OUTPUT);
+    pinMode(DIR, OUTPUT);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
+    pinMode(M1, OUTPUT);
+    pinMode(M2, OUTPUT);
+    pinMode(M3, OUTPUT);
 
-  pinMode(BUTTON1, INPUT_PULLUP);
-  pinMode(BUTTON2, INPUT_PULLUP);
-  pinMode(BUTTON3, INPUT_PULLUP);
-  pinMode(BUTTON4, INPUT_PULLUP);
+    pinMode(BUTTON1, INPUT_PULLUP);
+    pinMode(BUTTON2, INPUT_PULLUP);
+    pinMode(BUTTON3, INPUT_PULLUP);
+    pinMode(BUTTON4, INPUT_PULLUP);
 
-  digitalWrite(EN, 0);
-  digitalWrite(DIR, 1);
-  digitalWrite(M1, 1);
-  digitalWrite(M2, 1);
-  digitalWrite(M3, 0);
+    digitalWrite(EN, 0);
+    digitalWrite(DIR, 1);
+    digitalWrite(M1, 1);
+    digitalWrite(M2, 1);
+    digitalWrite(M3, 0);
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 
 }
 
 void loop() {
 
-  static int speed = 200;
-  int old_speed = speed;
+    static int speed = 200;
+    int old_speed = speed;
 
-  if (digitalRead(BUTTON1)==0) speed = 0;
-  if (digitalRead(BUTTON2)==0) speed = 200;
-  if (digitalRead(BUTTON3)==0) speed = 400;
-  if (digitalRead(BUTTON4)==0) speed = 600;
+    if (digitalRead(BUTTON1)==0) speed = 0;
+    if (digitalRead(BUTTON2)==0) speed = 100;
+    if (digitalRead(BUTTON3)==0) speed = 200;
+    if (digitalRead(BUTTON4)==0) speed = 300;
 
-  if (old_speed != speed) {
-    Timer1.setPeriod(1000000 / speed);
-    Serial.println(speed);
-  }
+    if (old_speed != speed) {
+        Timer1.setPeriod(1000000 / speed / 2);
+        Serial.println(speed);
+    }
 
-  digitalWrite(LED1, speed==0);
-  digitalWrite(LED2, speed==200);
-  digitalWrite(LED3, speed==400);
-  digitalWrite(LED4, speed==600);
+    digitalWrite(LED4, speed==0);
+    digitalWrite(LED3, speed==100);
+    digitalWrite(LED2, speed==200);
+    digitalWrite(LED1, speed==300);
 
-  delay(50);
+    delay(50);
 }
 
 void isr() {
-
-  digitalWrite(STEP, 1);
-  digitalWrite(LED1, 1);
-  delay(2);
-  digitalWrite(STEP, 0);
-  digitalWrite(LED1, 0);
+    static bool toggle = false;
+    digitalWrite(STEP, toggle);
+    toggle = !toggle;
 }
