a9467d47f88828ca2c442fdf370a62d9bbde2cec
Florin Tobler <florin.tobler@hotmail.com>
2022-10-01 20:01:44 +0200

    adjusted temperature, controller and oversampling

--- heater/src/main.cpp
+++ heater/src/main.cpp
@@ -3,23 +3,26 @@
 // Pins
 #define PWMOUT 3
 #define ADCIN A1
+#define LED 13
 
 // Control sample period
 #define DT_MS 100
 
-#define TARGET_TEMP 200
+#define TARGET_TEMP 185
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  // analogReference(EXTERNAL);
+    pinMode(LED, OUTPUT);
 
-  pinMode(PWMOUT, OUTPUT);
-  TCCR2B = (TCCR2B & B11111000) | B00000111; // for PWM frequency of 31372.55 Hz
+    // analogReference(EXTERNAL);
 
-  analogWrite(PWMOUT, 0);
+    pinMode(PWMOUT, OUTPUT);
+    TCCR2B = (TCCR2B & B11111000) | B00000111; // for PWM frequency of 31372.55 Hz
+
+    analogWrite(PWMOUT, 0);
 }
 
 #define T0 (25.0f + 273.15f)
@@ -28,57 +31,76 @@ void setup() {
 #define VCC 5.0f    //Supply voltage
 #define R 10000.0f  //R=10KΩ
 
+
+float analogReadMulti(int adc, unsigned long ms) {
+    uint32_t start = micros();
+    float sum = 0.0f;
+    int n = 0;
+    while (ms > 0) {
+        yield();
+        while ( ms > 0 && (micros() - start) >= 1000) {
+            ms--;
+            start += 1000;
+        }
+        sum += analogRead(adc);
+        n++;
+    }
+    return sum / n;
+}
+
 float getTemp(void) {
 
-  static float filtered = 0;
+    static float filtered = 0;
 
-  float VRT = analogRead(ADCIN); //Acquisition analog value of VRT
+    float VRT = analogReadMulti(ADCIN, DT_MS); //Acquisition analog value of VRT
 
-  // Serial.println(VRT);
+    // Serial.println(VRT);
 
-  VRT = (5.00f / 1023.00f) * VRT; //Conversion to voltage
-  float VR = VCC - VRT;
-  float RT = VRT / (VR / R); //Resistance of RT
+    VRT = (5.00f / 1023.00f) * VRT; //Conversion to voltage
+    float VR = VCC - VRT;
+    float RT = VRT / (VR / R); //Resistance of RT
 
-  float ln = log(RT / RT0);
-  float TX = (1.0f / ((ln / B) + (1.0f / T0))); //Temperature from thermistor
+    float ln = log(RT / RT0);
+    float TX = (1.0f / ((ln / B) + (1.0f / T0))); //Temperature from thermistor
 
-  TX = TX - 273.15; //Conversion to Celsius
+    TX = TX - 273.15f; //Conversion to Celsius
 
-  if (filtered == 0) filtered = TX;
+    if (filtered == 0) filtered = TX;
 
-  filtered = 0.9 * filtered + 0.1 * TX;
+    filtered = 0.9f * filtered + 0.1f * TX;
 
-  return filtered;
+    return filtered;
 }
 
 // PI Control parameters
-#define P 0.0125
-#define I 0.0025
+#define P 0.0125f
+#define I 0.0004f //0.0025f
 
 void loop() {
 
-  static float i = 0;
+    static float i = 0;
 
-  float temp = getTemp();
+    float temp = getTemp();
 
-  float x = TARGET_TEMP - temp;
+    float x = TARGET_TEMP - temp;
 
-  i += ((((float)DT_MS)/1000) * I) * x;
-  if (i > 0.2) i = 0.2;
-  if (i < -0.2) i = -0.2;
+    i += ((((float)DT_MS)/1000) * I) * x;
+    if (i > 2.0f) i = 2.0f;
+    if (i < -2.0f) i = -2.0f;
 
-  float p = P * x;
+    float p = P * x;
 
-  float y = p + i;
+    float y = p + i;
 
-  if (y > 1) y = 1;
-  if (y < 0) y = 0;
+    if (y > 1) y = 1;
+    if (y < 0) y = 0;
 
-  int pwm = y * 255;
-  analogWrite(PWMOUT, pwm);
+    int pwm = y * 255;
+    analogWrite(PWMOUT, pwm);
 
-  Serial.println("temp: " + ((String)temp) + " pwm: " + ((String)pwm) + " x: " + ((String)x) + " p: " + ((String)p) + " i: " + ((String)i));
+    Serial.println("temp: " + ((String)temp) + " pwm: " + ((String)pwm) + " x: " + ((String)x) + " p: " + ((String)p) + " i: " + ((String)i));
 
-  delay(DT_MS);
+    digitalWrite(LED, abs(x) < 3); //light up led if target reached
+
+    //delay(DT_MS);
 }
